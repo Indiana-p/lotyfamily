9bad2144561aec70b7fc1fe5b8a3ea8e48166c36
Indiana-p <philippe.loty+github@gmail.com>
2023-01-27 20:47:25 +0100

    correction

--- testmail.php
+++ testmail.php
@@ -4,7 +4,7 @@ $subject = "Test mail PHP";
 $content = "contenu";
 $headers = "From: Website <SendingEmail@lotyfamily.com>\r\nReply-To: SendingEmail@lotyfamily.com";
 
-if (mail($to, $subject, $_POST['nom']), $headers))
+if (mail($to, $subject, $_POST['nom'], $headers))
 echo "The email has been sent successfully!";
 else
 echo "Email did not leave correctly!";
